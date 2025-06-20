7ec5af2680ef533b720144ab5c6581faf0cfc95c
DmitryVah <zonadima@gmail.com>
2025-06-20 16:59:50 +0500

    Update settings.example.php

--- settings.example.php
+++ settings.example.php
@@ -5,6 +5,6 @@ const DB_NAME = 'domains';
 const DB_USER = 'root';
 const DB_PASS ='';
 // Ключ бота telegram
-const TOKEN_BOT = '705005157:AAEddFrhy7e3UH372jQEFyjsnHg3Gzo2Tag';
+const TOKEN_BOT = getenv('TOKEN_BOT');
 // Использовать Прокси tor, для обхода РКН?
 const PROXY_TOR = false;
