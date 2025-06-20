82d9861708e7442852557fef5e5cb9862dc061e8
DmitryVah <zonadima@gmail.com>
2025-06-20 17:04:32 +0500

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,12 @@
+<?php
+// Настройки подключения к базе данных.
+const DB_HOST = 'localhost';
+const DB_NAME = 'domains';
+const DB_USER = 'root';
+const DB_PASS ='';
+
+// Ключ бота telegram
+const TOKEN_BOT = getenv('TOKEN_BOT');
+
+// Использовать Прокси tor, для обхода РКН?
+const PROXY_TOR = false;
